5a5e36293e1d79b47acce510909d3fc6921437d3
Matthew Teolis <matthewteolis@gmail.com>
2018-02-13 19:02:20 -0500

    Implemented prototype functionality manage tags
    Added basic functionality to add, edit, and delete tags.
    There is no database interaction still.

---
+++ app/src/main/java/org/liberty/android/fantastischmemo/entity/Tag.java
@@ -0,0 +1,17 @@
+package org.liberty.android.fantastischmemo.entity;
+
+public class Tag {
+    private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/org/liberty/android/fantastischmemo/ui/TagsActivity.java
+++ app/src/main/java/org/liberty/android/fantastischmemo/ui/TagsActivity.java
@@ -1,6 +1,11 @@
 package org.liberty.android.fantastischmemo.ui;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.media.Image;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,10 +14,13 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.liberty.android.fantastischmemo.R;
 import org.liberty.android.fantastischmemo.common.BaseActivity;
+import org.liberty.android.fantastischmemo.entity.Tag;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,34 +31,77 @@ public class TagsActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_tags_layout);
 
-        RecyclerView tagsRecyclerView = (RecyclerView) findViewById(R.id.tags_list);
+        final RecyclerView tagsRecyclerView = (RecyclerView) findViewById(R.id.tags_list);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         tagsRecyclerView.setLayoutManager(linearLayoutManager);
         tagsRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
 
-        List<String> tags = new ArrayList<>();
+        final List<Tag> tags = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
-            tags.add("Some Tag Name " + (i + 1));
+            tags.add(new Tag("Some Tag Name " + (i + 1)));
         }
 
-        tagsRecyclerView.setAdapter(new TagsAdapter(tags));
+        final TagsAdapter tagsAdapter = new TagsAdapter(tags);
+        tagsRecyclerView.setAdapter(tagsAdapter);
+
+        FloatingActionButton addTagButton = (FloatingActionButton) findViewById(R.id.add_tag_fab);
+        addTagButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText input = new EditText(v.getContext());
+                new AlertDialog.Builder(v.getContext())
+                        .setTitle("Create Tag")
+                        .setMessage("Enter new name for tag below.")
+                        .setView(input)
+                        .setPositiveButton("Create", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String tagName = input.getText().toString();
+                                tagsAdapter.addTag(new Tag(tagName));
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+            }
+        });
     }
 
     class TagsAdapter extends RecyclerView.Adapter<TagsAdapter.TagsViewHolder> {
 
         class TagsViewHolder extends RecyclerView.ViewHolder {
             TextView tagTextView;
+            ImageButton tagEditButton;
 
             TagsViewHolder(View itemView) {
                 super(itemView);
                 tagTextView = (TextView) itemView.findViewById(R.id.tag_text_view);
+                tagEditButton = (ImageButton) itemView.findViewById(R.id.tag_edit_button);
             }
         }
 
-        private List<String> tags;
+        private List<Tag> tags;
 
-        TagsAdapter(List<String> tags) {
+        TagsAdapter() {
+            this.tags = new ArrayList<>();
+        }
+
+        TagsAdapter(List<Tag> tags) {
             this.tags = tags;
+        }
+
+        public void addTag(Tag tag) {
+            tags.add(tag);
+            this.notifyDataSetChanged();
+        }
+
+        private void deleteTag(int position) {
+            tags.remove(position);
+            this.notifyItemRemoved(position);
         }
 
         @Override
@@ -60,9 +111,56 @@ public class TagsActivity extends BaseActivity {
         }
 
         @Override
-        public void onBindViewHolder(TagsViewHolder holder, int position) {
-            String tag = tags.get(position);
-            holder.tagTextView.setText(tag);
+        public void onBindViewHolder(TagsViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+            final Tag tag = tags.get(position);
+            holder.tagTextView.setText(tag.getName());
+            holder.tagEditButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(final View v) {
+                    final EditText input = new EditText(v.getContext());
+                    input.setText(tag.getName());
+                    new AlertDialog.Builder(v.getContext())
+                            .setTitle(v.getContext().getString(R.string.edit_text))
+                            .setMessage("Enter new name for tag below.")
+                            .setView(input)
+                            .setPositiveButton(v.getContext().getString(R.string.settings_save), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    String tagName = input.getText().toString();
+                                    tag.setName(tagName);
+                                    TagsAdapter.this.notifyItemChanged(position);
+                                }
+                            })
+                            .setNegativeButton(v.getContext().getString(R.string.cancel_text), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            })
+                            .setNeutralButton(v.getContext().getString(R.string.delete_text), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    new AlertDialog.Builder(v.getContext())
+                                            .setTitle(v.getContext().getString(R.string.delete_text))
+                                            .setMessage("Are you sure you want to delete this tag?")
+                                            .setPositiveButton(v.getContext().getString(R.string.delete_text), new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    deleteTag(position);
+                                                }
+                                            })
+                                            .setNegativeButton(v.getContext().getString(R.string.cancel_text), new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    dialog.dismiss();
+                                                }
+                                            })
+                                            .show();
+                                }
+                            })
+                            .show();
+                }
+            });
         }
 
         @Override
