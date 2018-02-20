85bf8ebb46a315838af95df6d3fd76909f63e037
brendanmcgarry <brendanmcgarry13@gmail.com>
2018-02-20 16:22:44 -0500

    Deck Tags Filter: Logic to dynamically add Tags to Filter dropdown.

--- app/src/main/java/org/liberty/android/fantastischmemo/ui/RecentListFragment.java
+++ app/src/main/java/org/liberty/android/fantastischmemo/ui/RecentListFragment.java
@@ -42,6 +42,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -53,6 +54,7 @@ import org.liberty.android.fantastischmemo.common.AnyMemoDBOpenHelper;
 import org.liberty.android.fantastischmemo.common.AnyMemoDBOpenHelperManager;
 import org.liberty.android.fantastischmemo.common.BaseFragment;
 import org.liberty.android.fantastischmemo.dao.CardDao;
+import org.liberty.android.fantastischmemo.entity.DeckMap;
 import org.liberty.android.fantastischmemo.ui.helper.SelectableAdapter;
 import org.liberty.android.fantastischmemo.utils.DatabaseUtil;
 import org.liberty.android.fantastischmemo.utils.RecentListActionModeUtil;
@@ -74,6 +76,8 @@ public class RecentListFragment extends BaseFragment {
     private final AtomicInteger recentListVersion = new AtomicInteger(0);
 
     private final static String TAG = RecentListFragment.class.getSimpleName();
+
+    private static int tagCount = 0;
 
     @Inject RecentListUtil recentListUtil;
 
@@ -157,23 +161,38 @@ public class RecentListFragment extends BaseFragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
-        super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.open_screen_menu, menu);
+        menu.addSubMenu(Menu.NONE, R.id.filter_menu, Menu.NONE, "");
+        SubMenu filter = menu.findItem(R.id.filter_menu).getSubMenu();
+
+        MenuItem filterWrapper = filter.getItem();
+        filterWrapper.setIcon(R.drawable.ic_menu_filter);
+        filterWrapper.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        
+        filter.clear();
+        String[] tags = {"what", "huh"}; // TODO replace with DeckMap for now (DeckDao later)
+        for(/*tagCount*/; tagCount < tags.length; tagCount++) {
+            filter.add(0, Menu.FIRST + tagCount, Menu.NONE, tags[tagCount]);
+            MenuItem tagOption = filter.findItem(Menu.FIRST + tagCount);
+            tagOption.setCheckable(true);
+        }
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.openmenu_clear:
-                recentListUtil.clearRecentList();
-                onResume();
-                return true;
-            case R.id.filter_test1:
-            case R.id.filter_test2:
-                item.setChecked(!item.isChecked());
-                return false;
+        int itemId = item.getItemId();
+        if(itemId == R.id.openmenu_clear) {
+            recentListUtil.clearRecentList();
+            onResume();
+            return true;  // Prevent default on-selected action
         }
-        return false;
+        // Dynamic count of checkable elements in Tag Filter
+        else if(itemId > 0 && itemId <= tagCount) {
+            item.setChecked(!item.isChecked());
+            return false;
+        }
+        return false;  // Default case: Allow default on-selected action to occur
     }
 
     /** Aux class to store data */
