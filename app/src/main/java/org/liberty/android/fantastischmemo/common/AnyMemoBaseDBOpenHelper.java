a893c3471e9a51f04a7fdcbd63146b1ad7b10bdb
Olivier Nourry <onourry@hotmail.fr>
2018-03-01 15:32:02 -0500

    Refactored new database code to reuse existing code

---
+++ app/src/main/java/org/liberty/android/fantastischmemo/common/AnyMemoBaseDBOpenHelper.java
@@ -0,0 +1,143 @@
+package org.liberty.android.fantastischmemo.common;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.support.DatabaseConnection;
+import com.j256.ormlite.table.TableUtils;
+
+import org.liberty.android.fantastischmemo.dao.DeckDao;
+import org.liberty.android.fantastischmemo.dao.TagDao;
+import org.liberty.android.fantastischmemo.entity.Deck;
+import org.liberty.android.fantastischmemo.entity.Tag;
+
+import java.sql.SQLException;
+
+/**
+ * Created by Olivier on 2018-03-01.
+ */
+
+public class AnyMemoBaseDBOpenHelper extends OrmLiteSqliteOpenHelper {
+
+    private final String TAG = getClass().getSimpleName();
+
+    private static final int CURRENT_VERSION = 5;
+
+    private final String dbPath;
+
+    private DeckDao deckDao = null;
+
+    private TagDao tagDao = null;
+
+    private boolean isReleased = false;
+
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+        Log.v(TAG, "Now we are creating a new database!");
+        Log.i(TAG, "Newly created db version: " + database.getVersion());
+
+        try {
+            TableUtils.createTable(connectionSource, Deck.class);
+            TableUtils.createTable(connectionSource, Tag.class);
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Database creation error: " + e.toString());
+        }
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+        Log.v(TAG, "Old version" + oldVersion + " new version: " + newVersion);
+        // Update possible card with null category field
+        if (oldVersion <= 5) {
+            try {
+                database.execSQL("alter table decks add column dbPath VARCHAR");
+                TableUtils.createTable(connectionSource, Tag.class);
+            } catch (SQLException e) {
+                Log.e(TAG, "Upgrading failed, the tags table might already exist.", e);
+            } finally {
+                oldVersion = 5;
+            }
+        }
+        database.setVersion(oldVersion);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(TAG, String.format("Downgrading database from version %1$d to %2$d", oldVersion, newVersion));
+    }
+
+    /**
+     * Do not call this method directly, use AnyMemoBaseDBOpenHelperManager instead.
+     */
+    @Override
+    public void close() {
+        isReleased = true;
+        try {
+            DatabaseConnection connection = getConnectionSource().getReadWriteConnection();
+            getConnectionSource().releaseConnection(connection);
+        } catch (SQLException e) {
+            Log.e(TAG, "Error releasing the connection.", e);
+        }
+        super.close();
+    }
+
+    public synchronized DeckDao getDeckDao() {
+        try {
+            if (deckDao == null) {
+                deckDao = getDao(Deck.class);
+            }
+            return deckDao;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public synchronized TagDao getTagDao() {
+        try {
+            if (tagDao == null) {
+                tagDao = getDao(Tag.class);
+            }
+            return tagDao;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*
+     * Override the finalize in case the helper is not release.
+     */
+    @Override
+    public void finalize() throws Throwable {
+        super.finalize();
+        // If the finalize kicked in before the db is released.
+        // force release the helper!
+        // This is usually a bug in program.
+        if (!isReleased) {
+            Log.w(TAG, "AnyMemoBaseDBOpenHelper for db " + dbPath + " is not released before being GCed. This class must be explicitly released! Force releasing now.");
+            AnyMemoBaseDBOpenHelperManager.forceRelease(dbPath);
+        }
+    }
+
+    /* Package private constructor used in Manager. */
+    AnyMemoBaseDBOpenHelper(Context context, String dbpath) {
+        // R.raw.ormlite_config is used to accelerate the DAO creation.
+        super(context, dbpath, null, CURRENT_VERSION);
+        this.dbPath = dbpath;
+    }
+
+    /* Package private getDbPath used in Manager. */
+    String getDbPath() {
+        return dbPath;
+    }
+
+}
---
+++ app/src/main/java/org/liberty/android/fantastischmemo/utils/GenericDatabaseDialogUtil.java
@@ -0,0 +1,177 @@
+package org.liberty.android.fantastischmemo.utils;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.EditText;
+
+import org.apache.commons.io.FileUtils;
+import org.liberty.android.fantastischmemo.R;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import io.reactivex.Maybe;
+import io.reactivex.MaybeEmitter;
+import io.reactivex.MaybeOnSubscribe;
+
+/**
+ * Created by Olivier on 2018-03-01.
+ */
+
+public class GenericDatabaseDialogUtil {
+
+    private Activity activity;
+    private AMFileUtil amFileUtil;
+    private RecentListUtil recentListUtil;
+
+    @Inject
+    public GenericDatabaseDialogUtil(@NonNull final Activity activity,
+                                     @NonNull final AMFileUtil amFileUtil,
+                                     @NonNull final RecentListUtil recentListUtil) {
+        this.activity = activity;
+        this.amFileUtil = amFileUtil;
+        this.recentListUtil = recentListUtil;
+    }
+
+    public Maybe<File> showDeleteDbDialog(final File clickedFile) {
+        return Maybe.create(new MaybeOnSubscribe<File>() {
+            @Override
+            public void subscribe(final MaybeEmitter<File> emitter) throws Exception {
+                new AlertDialog.Builder(activity)
+                        .setTitle(activity.getString(R.string.delete_text))
+                        .setMessage(activity.getString(R.string.fb_delete_message))
+                        .setPositiveButton(activity.getString(R.string.delete_text), new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int which ){
+                                amFileUtil.deleteDbSafe(clickedFile.getAbsolutePath());
+                                emitter.onSuccess(clickedFile);
+                            }
+                        })
+                        .setNegativeButton(activity.getString(R.string.cancel_text), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                emitter.onComplete();
+
+                            }
+                        })
+                        .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                            @Override
+                            public void onCancel(DialogInterface dialog) {
+                                emitter.onComplete();
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+        });
+    }
+
+    public Maybe<File> showCloneDbDialog(final File clickedFile) {
+        return Maybe.create(new MaybeOnSubscribe<File>() {
+            @Override
+            public void subscribe(MaybeEmitter<File> emitter) throws Exception {
+                String srcDir = clickedFile.getAbsolutePath();
+                String destDir = srcDir.replaceAll(".db", ".clone.db");
+                try {
+                    FileUtils.copyFile(new File(srcDir), new File(destDir));
+                    emitter.onSuccess(new File(destDir));
+                } catch(IOException e){
+                    new AlertDialog.Builder(activity)
+                            .setTitle(activity.getString(R.string.fail))
+                            .setMessage(activity.getString(R.string.fb_fail_to_clone) + "\nError: " + e.toString())
+                            .setPositiveButton(activity.getString(R.string.ok_text), null)
+                            .create()
+                            .show();
+                    emitter.onComplete();
+                }
+            }
+        });
+    }
+
+    public Maybe<File> showRenameDbDialog(final File clickedFile) {
+        return Maybe.create(new MaybeOnSubscribe<File>() {
+            @Override
+            public void subscribe(final MaybeEmitter<File> emitter) throws Exception {
+                final EditText input = new EditText(activity);
+                input.setText(clickedFile.getAbsolutePath());
+                new AlertDialog.Builder(activity)
+                        .setTitle(activity.getString(R.string.fb_rename))
+                        .setMessage(activity.getString(R.string.fb_rename_message))
+                        .setView(input)
+                        .setPositiveButton(activity.getString(R.string.ok_text), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String value = input.getText().toString();
+                                if (!value.equals(clickedFile.getAbsolutePath())) {
+                                    try {
+                                        FileUtils.copyFile(clickedFile, new File(value));
+                                        amFileUtil.deleteDbSafe(clickedFile.getAbsolutePath());
+                                        recentListUtil.deleteFromRecentList(clickedFile.getAbsolutePath());
+                                        emitter.onSuccess(new File(value));
+                                    } catch (IOException e) {
+                                        new AlertDialog.Builder(activity)
+                                                .setTitle(activity.getString(R.string.fail))
+                                                .setMessage(activity.getString(R.string.fb_rename_fail) + "\nError: " + e.toString())
+                                                .setNeutralButton(activity.getString(R.string.ok_text), null)
+                                                .create()
+                                                .show();
+                                        emitter.onComplete();
+                                    }
+                                }
+                            }
+                        })
+                        .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                            @Override
+                            public void onCancel(DialogInterface dialog) {
+                                emitter.onComplete();
+                            }
+                        })
+                        .show();
+
+            }
+        });
+    }
+
+    public Maybe<File> showCreateFolderDialog(@NonNull final File currentDirectory) {
+        return Maybe.create(new MaybeOnSubscribe<File>() {
+            @Override
+            public void subscribe(final MaybeEmitter<File> emitter) throws Exception {
+                final EditText input = new EditText(activity);
+                new AlertDialog.Builder(activity)
+                        .setTitle(R.string.fb_create_dir)
+                        .setMessage(R.string.fb_create_dir_message)
+                        .setView(input)
+                        .setPositiveButton(activity.getString(R.string.ok_text), new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int which ){
+                                String value = input.getText().toString();
+                                File newDir = new File(currentDirectory.getAbsolutePath() + "/" + value);
+                                newDir.mkdir();
+                                emitter.onSuccess(newDir);
+                            }
+                        })
+                        .setNegativeButton(activity.getString(R.string.cancel_text), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                emitter.onComplete();
+                            }
+                        })
+                        .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                            @Override
+                            public void onCancel(DialogInterface dialog) {
+                                emitter.onComplete();
+
+                            }
+                        })
+                        .create()
+                        .show();
+
+            }
+        });
+    }
+}
