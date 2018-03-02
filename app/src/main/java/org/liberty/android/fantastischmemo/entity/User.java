5f2c20d0a16730f5ef60996fb788c04d1440eed8
prich28 <paulrichard.28@gmail.com>
2018-03-01 19:33:38 -0500

    added setters and getters for attributes in both UserStatistics class and User class, as per task issue: #55 and story: #52

--- app/src/main/java/org/liberty/android/fantastischmemo/entity/User.java
+++ app/src/main/java/org/liberty/android/fantastischmemo/entity/User.java
@@ -3,6 +3,7 @@ package org.liberty.android.fantastischmemo.entity;
 
 
 
+import com.j256.ormlite.field.DataType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
@@ -17,6 +18,7 @@ import java.util.Date;
 @DatabaseTable(tableName = "user", daoClass = UserDaoImpl.class)
 public class User implements VersionableDomainObject{
 
+    //Attributes
     @DatabaseField(generatedId = true)
     private Integer id;
 
@@ -30,26 +32,76 @@ public class User implements VersionableDomainObject{
     private String username;
 
     @DatabaseField(foreign = true)
-    private UserStatistics userStats;
+    private UserStatistics userStatistics;
+
+    @DatabaseField(format="yyyy-MM-dd HH:mm:ss.SSSSSS", dataType= DataType.DATE_STRING)
+    private Date creationDate;
+
+    @DatabaseField(format="yyyy-MM-dd HH:mm:ss.SSSSSS", dataType=DataType.DATE_STRING)
+    private Date updateDate;
 
 
+    //Setters and Getters
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UserStatistics getUserStatistics() {
+        return this.userStatistics;
+    }
+
+    public void setUserStatistics(UserStatistics userStatistics) {
+        this.userStatistics = userStatistics;
+    }
 
     public Date getCreationDate() {
-        return null;
+        return creationDate;
     }
-
 
     public void setCreationDate(Date creationDate) {
-
+        this.creationDate = creationDate;
     }
-
 
     public Date getUpdateDate() {
-        return null;
+        return updateDate;
     }
 
-
     public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
 
+    @Override
+    public String toString(){
+        return "User [id=" + id + ", surname=" + surname
+                + ", name=" + name + ", username=" + username
+                + ", userStatistics=" + userStatistics +  "]";
     }
 }
--- app/src/main/java/org/liberty/android/fantastischmemo/entity/UserStatistics.java
+++ app/src/main/java/org/liberty/android/fantastischmemo/entity/UserStatistics.java
@@ -16,14 +16,15 @@ import java.util.Date;
 @DatabaseTable(tableName = "userstatistics", daoClass = UserStatisticsDaoImpl.class)
 public class UserStatistics implements VersionableDomainObject{
 
+    //Attributes
     @DatabaseField(generatedId = true)
-    private Integer statisticsId;
+    private Integer id;
 
     @DatabaseField(version = true, format="yyyy-MM-dd HH:mm:ss.SSSSSS", dataType= DataType.DATE_STRING)
     private Date lastLogin;
 
     @DatabaseField(foreign = true)
-    private Integer pointsId;
+    private Points points;
 
     @DatabaseField(generatedId = true)
     private Integer multiplier;
@@ -41,25 +42,113 @@ public class UserStatistics implements VersionableDomainObject{
     private Integer months;
 
     @DatabaseField(foreign = true)
-    private int userId;
+    private User user;
+
+    @DatabaseField(format="yyyy-MM-dd HH:mm:ss.SSSSSS", dataType= DataType.DATE_STRING)
+    private Date creationDate;
+
+    @DatabaseField(format="yyyy-MM-dd HH:mm:ss.SSSSSS", dataType=DataType.DATE_STRING)
+    private Date updateDate;
+
+
+    //Getters and Setters
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getLastLogin() {
+        return this.lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public Points getPoints() {
+        return points;
+    }
+
+    public void setPoints(Points points) {
+        this.points = points;
+    }
+
+    public Integer getMultiplier() {
+        return multiplier;
+    }
+
+    public void setMultiplier(Integer multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    public Integer getStreak() {
+        return streak;
+    }
+
+    public void setStreak(Integer streak) {
+        this.streak = streak;
+    }
+
+    public Integer getLongestStreak() {
+        return longestStreak;
+    }
+
+    public void setLongestStreak(Integer longestStreak) {
+        this.longestStreak = longestStreak;
+    }
+
+    public Integer getWeeks() {
+        return weeks;
+    }
+
+    public void setWeeks(Integer weeks) {
+        this.weeks = weeks;
+    }
+
+    public Integer getMonths() {
+        return months;
+    }
+
+    public void setMonths(Integer months) {
+        this.months = months;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     @Override
     public Date getCreationDate() {
-        return null;
+        return creationDate;
     }
 
     @Override
     public void setCreationDate(Date creationDate) {
-
+        this.creationDate = creationDate;
     }
 
     @Override
     public Date getUpdateDate() {
-        return null;
+        return updateDate;
     }
 
     @Override
     public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
 
+    @Override
+    public String toString(){
+        return "User [id=" + id + ", lastLogin=" + lastLogin
+                + ", multiplier=" + multiplier + ", streak=" + streak
+                + ", longestStreak=" + longestStreak + ",weeks=" + weeks
+                + ", months" + months + ", user" + user + "]";
     }
 }
