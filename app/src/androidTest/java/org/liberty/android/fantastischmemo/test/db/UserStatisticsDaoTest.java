ded9bba2762522f5a94a9a4d0fe4df6120517ba6
prich28 <paulrichard.28@gmail.com>
2018-03-15 01:21:23 -0400

    removed useless import on UserStatistcsDaoTest. task issue #86 and story issue #52 - same issues as commit b492d3a7

--- app/src/androidTest/java/org/liberty/android/fantastischmemo/test/db/UserStatisticsDaoTest.java
+++ app/src/androidTest/java/org/liberty/android/fantastischmemo/test/db/UserStatisticsDaoTest.java
@@ -9,7 +9,6 @@ import org.liberty.android.fantastischmemo.dao.UserStatisticsDao;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
 /**
