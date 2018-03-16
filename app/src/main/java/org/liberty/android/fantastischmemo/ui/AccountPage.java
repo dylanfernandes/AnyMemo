4298d6785bf0613901013c45e4b69ba70eba7131
DESKTOP-75EJLIQ\Hiu Tung Lam Emily <e.mily.lam@hotmail.com>
2018-03-16 00:53:28 -0400

    - created account page - clicking the account button on the left menu will lead to account page (issue #107)

---
+++ app/src/main/java/org/liberty/android/fantastischmemo/ui/AccountPage.java
@@ -0,0 +1,18 @@
+package org.liberty.android.fantastischmemo.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.liberty.android.fantastischmemo.R;
+import org.liberty.android.fantastischmemo.common.BaseActivity;
+
+/**
+ * Created by Emily on 2018-03-15.
+ */
+
+public class AccountPage extends BaseActivity{
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_page_tab);
+    };
+}
