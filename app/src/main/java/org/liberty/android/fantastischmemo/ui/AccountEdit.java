5ba354bab126cbdf2c7f10209ea5a023feed9ecd
Olivier Nourry <onourry@hotmail.fr>
2018-03-20 14:39:27 -0400

    #110 Removed unused imports

--- app/src/main/java/org/liberty/android/fantastischmemo/ui/AccountEdit.java
+++ app/src/main/java/org/liberty/android/fantastischmemo/ui/AccountEdit.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 
 import org.liberty.android.fantastischmemo.R;
 import org.liberty.android.fantastischmemo.common.AMEnv;
