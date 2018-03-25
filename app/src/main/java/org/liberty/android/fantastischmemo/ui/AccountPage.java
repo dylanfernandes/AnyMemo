d7cb96ba0299e489bb8f6294c95d76afbf31d883
DESKTOP-75EJLIQ\Hiu Tung Lam Emily <e.mily.lam@hotmail.com>
2018-03-24 20:36:08 -0400

    - created delete button to the header in account page - moved edit button to header too (Issue #143)

--- app/src/main/java/org/liberty/android/fantastischmemo/ui/AccountPage.java
+++ app/src/main/java/org/liberty/android/fantastischmemo/ui/AccountPage.java
@@ -3,6 +3,9 @@ package org.liberty.android.fantastischmemo.ui;
 import android.accounts.Account;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.app.Activity;
@@ -19,6 +22,7 @@ import org.liberty.android.fantastischmemo.entity.User;
 import org.liberty.android.fantastischmemo.entity.UserStatistics;
 import org.liberty.android.fantastischmemo.dao.UserDao;
 
+import java.util.Collection;
 
 /**
  * Created by Emily on 2018-03-15.
@@ -51,7 +55,8 @@ public class AccountPage extends BaseActivity{
         userDao = baseHelper.getUserDao();
         userStatDao = baseHelper.getUserStatisticsDao();
 
-        if(userDao.queryForId(1) == null){
+        Collection<User> userlist = userDao.queryForAll();
+        if(userlist.size() == 0){
             User defaultUser = userDao.createOrReturn("DefaultUsername");
             defaultUser.setName("DefaultName");
             defaultUser.setSurname("DefaultSurname");
@@ -59,8 +64,18 @@ public class AccountPage extends BaseActivity{
             userStatDao.createOrReturn(defaultUser);
         }
 
-        user = userDao.queryForId(1);
-        userStat = userStatDao.queryForId(1);
+        /*
+        if(userDao.queryForId(1) == null){
+            User defaultUser = userDao.createOrReturn("DefaultUsername");
+            defaultUser.setName("DefaultName");
+            defaultUser.setSurname("DefaultSurname");
+            userDao.update(defaultUser);
+            userStatDao.createOrReturn(defaultUser);
+        }
+        */
+
+        user = userDao.createOrReturn("DefaultUsername");
+        userStat = userStatDao.createOrReturn(user);
 
         user_Name = (TextView)findViewById(R.id.account_user_name);
         user_Name.setText(user.getName());
@@ -73,18 +88,32 @@ public class AccountPage extends BaseActivity{
 
         current_streak = (TextView)findViewById(R.id.account_current_streak);
         current_streak.setText(String.valueOf(userStat.getStreak()));
-
-        edit_button = (ImageButton)findViewById(R.id.edit_account);
-        edit_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AccountPage.this, AccountEdit.class);
-                intent.putExtra("CURRENT_USER_NAME",user_Name.getText().toString());
-                intent.putExtra("CURRENT_USERNAME",username.getText().toString());
-                startActivity(intent);}
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.account_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.edit_account: {
+                Intent intent = new Intent(AccountPage.this, AccountEdit.class);
+                intent.putExtra("CURRENT_USER_NAME", user_Name.getText().toString());
+                intent.putExtra("CURRENT_USERNAME", username.getText().toString());
+                startActivity(intent);
+                return true;
+            }
+            case R.id.delete_account:{
+                userDao.delete(user);
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
 
