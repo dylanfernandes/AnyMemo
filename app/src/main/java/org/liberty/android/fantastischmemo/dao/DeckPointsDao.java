66c418a80af5511b38301152db05485f66132fa6
Dylan Fernandes <dylangfernandes@gmail.com>
2018-04-13 10:44:19 -0400

    #190 creation of deckPointsDao

---
+++ app/src/main/java/org/liberty/android/fantastischmemo/dao/DeckPointsDao.java
@@ -0,0 +1,14 @@
+package org.liberty.android.fantastischmemo.dao;
+
+import com.j256.ormlite.dao.Dao;
+
+import org.liberty.android.fantastischmemo.entity.DeckPoints;
+import org.liberty.android.fantastischmemo.entity.User;
+
+/**
+ * Created by dylanfernandes on 2018-04-13.
+ */
+
+public interface DeckPointsDao extends Dao<DeckPoints, Integer> {
+
+}
